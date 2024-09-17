4bfa265ca833b635684091d5010433f9333a097c
miles20232131231 <gamingrobloxmilesplayz@outlook.com>
2024-09-17 10:18:36 +0200

    Update startup.js

--- commands/utility/startup.js
+++ commands/utility/startup.js
@@ -73,7 +73,7 @@ module.exports.handleInteraction = async (interaction) => {
     if (interaction.isButton() && interaction.customId === 'bannedVehicleList') {
         // Send the Google Docs link when the button is clicked
         await interaction.reply({
-            content: 'Here is the banned vehicle list document: https://docs.google.com/document/d/1hNoYMil7oEzHnIwUxXbAmop43zf8aoWQuA8T8WKQFlQ/edit?usp=sharing',
+            content: 'https://docs.google.com/document/d/1hNoYMil7oEzHnIwUxXbAmop43zf8aoWQuA8T8WKQFlQ/edit?usp=sharing',
             ephemeral: true  // Only the user who clicked the button will see this message
         });
     }
